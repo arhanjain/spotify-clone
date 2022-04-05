7f9b1978de4010f79d99d41ce56e99c09421f71e
arhanjain <arhan.j04@gmail.com>
2022-04-04 23:31:40 -0700

    context api from react to pass data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Login from './Login';
 import { getTokenFromURL } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
 
   const [token, setToken] = useState(null);
+  const [{user}, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromURL();
@@ -17,20 +20,25 @@ function App() {
     
     if (_token) {
       setToken(_token);
+
       spotify.setAccessToken(_token);
       spotify.getMe().then((user) => {
-        console.log(user);
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        })
       });
-      // console.log("lmaoo", temp);
     }
 
   }, []);
+
+  console.log("hehe", user);
 
   return (
     <div className="app">
       {
         token ? (
-          <h1>Logged in</h1>
+          <Player/>
         ) : (
           <Login />
         )
